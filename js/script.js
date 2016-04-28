bccc82638d5bd5ac3d35b0febbc4bd6a4a9bae21
Charles Duong <charlesduong14@gmail.com>
2016-04-27 23:49:57 -0500

    added footer minor fixes

--- js/script.js
+++ js/script.js
@@ -3,35 +3,32 @@ $( document ).ready(function() {
 $(".navbar").hide(); // hide the fixed navbar initially
 
 var headerHeight = $(".name").outerHeight();  // gets height of header
-var whoHeight = $(".card:first").outerHeight();  // gets height of header
-var lastScrollTop = 0;
+// var whoHeight = $(".card:first").outerHeight();  // gets height of header
+// var lastScrollTop = 0;
 
 $(window).scroll(function(){
 	// stick after header
-	if($(window).scrollTop() > (headerHeight-60)){
+	if($(window).scrollTop() > (headerHeight-45)){
 		$(".right-container").addClass('sticky');
-
-		// show after scroll past header
-		if($(window).scrollTop() > (headerHeight)){
-		   $(".navbar").show();
-
-		   	// hide again after scroll past who am i
-			if($(window).scrollTop() > (headerHeight + whoHeight)){
-			   $(".navbar").hide();
-			}
-			// animate back in if scrolling up
-			var st = $(this).scrollTop();
-			if (st < lastScrollTop){
-			   $(".navbar").show();
-			} 
-			lastScrollTop = st;
-		}
-		else {
-			$(".navbar").hide();
-		}
+		$(".navbar").slideDown(200);
+		// if($(window).scrollTop() > (headerHeight)){
+		//    	// hide again after scroll past who am i
+		// 	if($(window).scrollTop() > (headerHeight + whoHeight)){
+		// 	   $(".navbar").hide();
+		// 	}
+		// 	// animate back in if scrolling up
+		// 	var st = $(this).scrollTop();
+		// 	if (st < lastScrollTop){
+		// 	   $(".navbar").show();
+		// 	} 
+		// 	lastScrollTop = st;
+		// }
+		// else {
+		// }
 	}
 
 	else {
+		   $(".navbar").slideUp(200);
 		$(".right-container").removeClass('sticky');
 	}
 });

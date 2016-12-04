6adff0c2535238af7837c415a415c4bbf016ecd9
Charles Duong <charlesduong14@gmail.com>
2016-12-03 23:51:57 -0600

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+	<header className='vh-100 dt w-100 pa3 pa4-l'>
+		<div className='dtc v-btm-l v-mid-m v-top navy sans-serif bg-washed-blue ba b--light-blue pa3 pa4-l tc tl-ns'>
+			<h1 className='f2 fw1 ma0 tracked'>charles duong</h1>
+			<h2 className='f6 fw6 ttu measure bb bw2 b--light-blue pb2 ma0 tracked-mega'>product designer</h2>
+		</div>
+	</header>
+)

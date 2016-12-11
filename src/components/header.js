49f19258994c8d4885ade5a6f7c1cbd54184852b
Charles Duong <charlesduong14@gmail.com>
2016-12-11 16:01:15 -0600

    added responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default () => (
-	<header className='vh-100 dt w-100 pa3 pa4-l'>
-		<div className='dtc v-btm-l v-mid-m v-top navy sans-serif bg-washed-blue ba b--light-blue pa3 pa4-l tc tl-ns'>
+	<header className='vh-100 w-100 dt pa3 pa4-l'>
+		<div className='dtc v-mid v-btm-l navy sans-serif bg-washed-blue ba b--light-blue pa3 pa4-l tc tl-ns'>
 			<h1 className='f2 fw1 ma0 tracked'>charles duong</h1>
 			<h2 className='f6 fw6 ttu measure bb bw2 b--light-blue pb2 ma0 tracked-mega'>product designer</h2>
 		</div>

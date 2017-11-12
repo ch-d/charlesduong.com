340284a6cabfa86cc5006522ba1623461b0a3866
Charles Duong <charlesduong14@gmail.com>
2017-11-12 01:49:28 -0600

    added feathericon social icons

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,8 @@
 import baseStyles from '../constants/baseStyles';
-import Social from '../components/social';
 import styled from 'styled-components/';
 import { space } from 'styled-system';
 import { ThemeProvider } from 'styled-components'
+import Heading from '../components/heading';
 
 const theme = {
   breakpoints: [
@@ -51,33 +51,38 @@ const H2 = styled.h2`
   letter-spacing: .25em;
 `
 
+const Icolink= styled.a`
+  ${space}
+  &:hover {
+    border-bottom: 2px solid ${props => props.color}
+  }
+  &:hover svg {
+    stroke:${props => props.color}
+  };
+`
+
 export default () => (
-  <ThemeProvider theme={theme}>
-  	<White>
-  		<WhiteOffset p={[2,2,2,3]} m={[2,2,2,3]}>
-  			<H1>charles duong</H1>
-  			<H2>product designer</H2>
-        <Social
-          url='https://twitter.com/hicharlesd'
-          site='twitter'
-          color='1DA1F2'
-        />
-        <Social
-          url='https://linkedin.com/in/charlesduong'
-          site='linkedin'
-          color='0077B5'
-        />
-        <Social
-          url='https://github.com/ch-d'
-          site='github'
-          color='181717'
-        />
-        <Social
-          url='mailto:charles@charlesduong.com'
-          site='gmail'
-          color='D14836'
-        />
-  		</WhiteOffset>
-  	</White>
-  </ThemeProvider>
+  <div>
+    <Heading titleCase=""/>
+    <ThemeProvider theme={theme}>
+    	<White>
+    		<WhiteOffset p={[2,2,2,3]} m={[2,2,2,3]}>
+    			<H1>charles duong</H1>
+    			<H2>product designer</H2>
+          <Icolink href='https://twitter.com/hicharlesd' target='_blank' mr={1} color="#1DA1F2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7f959d" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+          </Icolink>
+          <Icolink href='https://github.com/ch-d' target='_blank' mx={1} color="#181717">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7f959d" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+          </Icolink>
+          <Icolink href='mailto:charles@charlesduong.com' target='_blank' mx={1} color="#D14836">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7f959d" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+          </Icolink>
+          <Icolink href='http://scapes.charlesduong.com' target='_blank' mx={1} color="#0077B5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7f959d" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-camera"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+          </Icolink>
+    		</WhiteOffset>
+    	</White>
+    </ThemeProvider>
+  </div>
 )

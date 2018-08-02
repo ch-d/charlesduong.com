7fdce88f64a00a01354e128262703a8c19335045
Charles Duong <charlesduong14@gmail.com>
2018-08-02 13:33:23 -0700

    updated hero copy spacing

--- components/hero.js
+++ components/hero.js
@@ -36,7 +36,10 @@ export default () => (
     </Box>
     <Box width={[1, 1/2, 1/3]} py={[0,2,3]} my={[0,2,4]}>
       <Paragraph mb={[4,2]}>
-        Hi, I’m Charles and I’m a product designer working on the world wide web. I do work on design systems, research, and tooling. Continuously interested in making technology a little more human, accessible, and inclusive. Currently available for full-time or contract opportunities.
+        Hi, I’m Charles and I’m a product designer working on the world wide web. I do work on design systems, research, and tooling. Continuously interested in making technology a little more human, accessible, and inclusive.
+      </Paragraph>
+      <Paragraph>
+        Currently available for full-time or contract opportunities.
       </Paragraph>
       <Social />
     </Box>

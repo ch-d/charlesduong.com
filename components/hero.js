003dafc65410a8e265f81f46015d06fd00a488ba
Charles Duong <charlesduong14@gmail.com>
2018-08-13 20:04:11 -0700

    initial pulse animation

--- components/hero.js
+++ components/hero.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Flex, Box } from 'grid-styled';
 import { space } from 'styled-system';
 import Social from './social';
@@ -28,6 +28,65 @@ const Paragraph = styled.p`
 	${space}
 `;
 
+const Pill = styled.span`
+  background-color: ${props => props.theme.colors.gradient.green};
+  color: white;
+  padding: .125rem .5rem;
+  border-radius: .25rem;
+`
+
+const pulse = keyframes`
+  0% {
+      opacity: .5;
+      transform: scale(1);
+  }
+  80% {
+      opacity: 0;
+      transform: scale(1.5);
+  }
+  100% {
+      opacity: 0;
+      transform: scale(2);
+  }
+`;
+
+const grow = keyframes`
+  0% {
+      transform: scale(.5);
+  }
+  80% {
+      transform: scale(.75);
+  }
+  100% {
+      transform: scale(1);
+  }
+`;
+
+const Pulse = styled.span`
+  display: inline-block;
+  position: relative;
+  top: -2px;
+  margin-right: .5rem;
+  width: .5rem;
+  height: .5rem;
+  border-radius: 50%;
+  background: #fff;
+  position: relative;
+  animation: ${grow} 2s ease 0s infinite;
+  &:after {
+    content:"";
+    display: block;
+    width: .75rem;
+    height: .75rem;
+    border-radius: 50%;
+    border: 2px solid #fff;
+    position:absolute;
+    top:-.25rem;
+    left:-.25rem;
+    animation: ${pulse} 2s ease 0s infinite;
+  }
+`
+
 export default () => (
 	<Flex wrap mx={[2,2,0]}>
     <Box width={[1, 1/2, 1/3]} ml={['0','0','16.7%']} py={[1,3,4]} my={[4,2,4]} pl={[0,3,0]}>
@@ -39,7 +98,7 @@ export default () => (
         Hi, I’m Charles and I’m a product designer working on the world wide web. I do work on design systems, research, and tooling. Continuously interested in making technology a little more human, accessible, and inclusive.
       </Paragraph>
       <Paragraph>
-        Currently available for full-time or contract opportunities. 
+        Currently <Pill><Pulse />available</Pill> for full-time or contract opportunities. 
       </Paragraph>
       <Social />
     </Box>
